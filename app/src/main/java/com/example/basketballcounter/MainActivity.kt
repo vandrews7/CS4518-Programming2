29343d636f1dd30f88ecaf00cf6a1120da0a62f8
vandrews7 <iamveronicaandrews@outlook.com>
2021-09-03 09:20:29 -0400

    added scores, buttons work

--- app/src/main/java/com/example/basketballcounter/MainActivity.kt
+++ app/src/main/java/com/example/basketballcounter/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.basketballcounter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,32 +14,45 @@ class MainActivity : AppCompatActivity() {
         val left3btn = findViewById<Button>(R.id.left3pointBut)
         val left2btn = findViewById<Button>(R.id.left2pointBut)
         val leftFTbtn = findViewById<Button>(R.id.leftFTBut)
+        var scoreA = 0
+        val txtScoreA = findViewById<TextView>(R.id.textScoreA)
         val right3btn = findViewById<Button>(R.id.right3pointBut)
         val right2btn = findViewById<Button>(R.id.right2pointBut)
         val rightFTbtn = findViewById<Button>(R.id.rightFTBut)
+        var scoreB = 0
+        val txtScoreB = findViewById<TextView>(R.id.textScoreB)
         val resetBtn = findViewById<Button>(R.id.resetBut)
 
         // handle the button clicks
         resetBtn.setOnClickListener {
-
+            scoreA = 0
+            scoreB = 0
+            txtScoreA.text = scoreA.toString()
+            txtScoreB.text = scoreB.toString()
         }
         left3btn.setOnClickListener {
-
+            scoreA += 3
+            txtScoreA.text = scoreA.toString()
         }
         left2btn.setOnClickListener {
-
+            scoreA += 2
+            txtScoreA.text = scoreA.toString()
         }
         leftFTbtn.setOnClickListener {
-
+            scoreA += 1
+            txtScoreA.text = scoreA.toString()
         }
         right3btn.setOnClickListener {
-
+            scoreB += 3
+            txtScoreB.text = scoreB.toString()
         }
         right2btn.setOnClickListener {
-
+            scoreB += 2
+            txtScoreB.text = scoreB.toString()
         }
         rightFTbtn.setOnClickListener {
-
+            scoreB += 1
+            txtScoreB.text = scoreB.toString()
         }
 
     }

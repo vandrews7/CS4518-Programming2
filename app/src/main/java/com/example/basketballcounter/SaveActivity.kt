241c1b59046dbf47b1f2a8a6417fd94b9c0bce7a
vandrews7 <iamveronicaandrews@outlook.com>
2021-09-17 14:50:05 -0400

    made the cool button do a thing

--- app/src/main/java/com/example/basketballcounter/SaveActivity.kt
+++ app/src/main/java/com/example/basketballcounter/SaveActivity.kt
@@ -2,10 +2,21 @@ package com.example.basketballcounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class SaveActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_save)
+
+        val coolButton = findViewById<Button>(R.id.coolBtn)
+        var coolText = findViewById<TextView>(R.id.coolTxt)
+
+        coolButton.setOnClickListener {
+            coolText.visibility = View.VISIBLE
+            coolButton.isClickable = false
+        }
     }
 }

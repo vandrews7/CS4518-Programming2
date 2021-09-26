ad7d73da615de33e9160b317873626378d975f8d
Maranda117 <mmallen@wpi.edu>
2021-09-26 11:08:47 -0400

    date attribute and button clicks

--- app/src/main/java/com/example/basketballcounter/Game.kt
+++ app/src/main/java/com/example/basketballcounter/Game.kt
@@ -7,5 +7,6 @@ data class Game(val id: UUID = UUID.randomUUID(),
                 var teamBname: String = "",
                 var scoreA: Int = 0,
                 var scoreB: Int = 0,
+                var date: Date = Date()
 ) {
 }
--- app/src/main/java/com/example/basketballcounter/GameFragment.kt
+++ app/src/main/java/com/example/basketballcounter/GameFragment.kt
@@ -1,5 +1,7 @@
 package com.example.basketballcounter
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,11 +9,17 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 
 class GameFragment: Fragment() {
+
+    private val scoreViewModel: ScoreViewModel by lazy {
+        ViewModelProviders.of(this).get(ScoreViewModel::class.java)
+    }
+
     private lateinit var game: Game
-    private lateinit var teamAfield: EditText
-    private lateinit var teamBfield: EditText
+    private lateinit var teamAname: EditText
+    private lateinit var teamBname: EditText
     private lateinit var teamAscore: TextView
     private lateinit var teamBscore: TextView
     private lateinit var Abtn3: Button
@@ -39,10 +47,11 @@ class GameFragment: Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_game, container, false)
 
-        teamAfield = view.findViewById(R.id.teamA) as EditText
-        teamBfield = view.findViewById(R.id.teamB) as EditText
+        teamAname = view.findViewById(R.id.teamA) as EditText
+        teamBname = view.findViewById(R.id.teamB) as EditText
         teamAscore = view.findViewById(R.id.scoreA) as TextView
         teamBscore = view.findViewById(R.id.scoreB) as TextView
+        winnerTxt = view.findViewById(R.id.winnerDisp) as TextView
         Abtn3 = view.findViewById(R.id.Apoints3) as Button
         Abtn2 = view.findViewById(R.id.Apoints2) as Button
         AbtnFree = view.findViewById(R.id.Afree) as Button
@@ -53,11 +62,92 @@ class GameFragment: Fragment() {
         resetBtn = view.findViewById(R.id.reset) as Button
         display = view.findViewById(R.id.displayBtn) as Button
         winnerBtn = view.findViewById(R.id.winner) as Button
-        winnerTxt = view.findViewById(R.id.winnerDisp) as TextView
+
+        Abtn3.setOnClickListener {
+            teamAscore.text = scoreViewModel.addScoreA(3)
+        }
+
+        Abtn2.setOnClickListener {
+            teamBscore.text = scoreViewModel.addScoreA(2)
+        }
+
+        AbtnFree.setOnClickListener {
+            teamBscore.text = scoreViewModel.addScoreA(1)
+        }
+
+        Bbtn3.setOnClickListener {
+            teamBscore.text = scoreViewModel.addScoreB(3)
+        }
+
+        Bbtn2.setOnClickListener {
+            teamBscore.text = scoreViewModel.addScoreB(2)
+        }
+
+        BbtnFree.setOnClickListener {
+            teamBscore.text = scoreViewModel.addScoreB(1)
+        }
+
+        resetBtn.setOnClickListener {
+            teamAscore.text = scoreViewModel.resetScoreA()
+            teamBscore.text = scoreViewModel.resetScoreB()
+            scoreViewModel.resetWinPressed()
+            winnerTxt.text = ""
+        }
 
         return view
     }
 
+    override fun onStart() {
+        super.onStart()
+
+        val teamAwatcher = object: TextWatcher{
+            override fun beforeTextChanged(sequence: CharSequence?,
+                                           start: Int,
+                                           count: Int,
+                                           after: Int
+            ) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTextChanged(sequence: CharSequence?,
+                                       start: Int,
+                                       count: Int,
+                                       after: Int
+            ) {
+                TODO("Not yet implemented")
+                game.teamAname = sequence.toString()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                TODO("Not yet implemented")
+            }
+        }
+        teamAname.addTextChangedListener(teamAwatcher)
+
+        val teamBwatcher = object: TextWatcher{
+            override fun beforeTextChanged(sequence: CharSequence?,
+                                           start: Int,
+                                           count: Int,
+                                           after: Int
+            ) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTextChanged(sequence: CharSequence?,
+                                       start: Int,
+                                       count: Int,
+                                       after: Int
+            ) {
+                TODO("Not yet implemented")
+                game.teamBname = sequence.toString()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                TODO("Not yet implemented")
+            }
+        }
+        teamBname.addTextChangedListener(teamBwatcher)
+    }
 
 
 }
